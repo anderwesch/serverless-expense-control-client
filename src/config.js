ee43369f940621e871738b8ade3fae992d45f31e
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2021-04-24 19:12:08 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://c3cp8lb0ha.execute-api.us-east-1.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_xDmiSdVMt",
+    APP_CLIENT_ID: "6fvdqiaeb8t5frvmn7fen2ceqm",
+    IDENTITY_POOL_ID: "us-east-1_xDmiSdVMt",
+  },
+};
+
+export default config;
